f025c00230745417e804a9b39bcbe79c46a6cbf0
Mitchell Gould <mitchellgould7@gmail.com>
2021-02-26 11:28:15 -0500

    can open and close menu

--- main/assets/js/scripts.js
+++ main/assets/js/scripts.js
@@ -321,28 +321,31 @@ Math.easeOutElastic = function (t, b, c, d) {
 function resetFocusTabsStyle() {
   window.dispatchEvent(new CustomEvent('initFocusTabs'));
 };
-  const nav = document.querySelector(".nav")
-  console.log(nav);
-  nav.addEventListener('click', (e) => {
-    nav.classList.toggle("open");
-    // e.currentTarget.classList.toggle(".open");
-  })
-  // // listen for key events
-  // window.addEventListener('keyup', function(event){
-  //   // listen for esc key
-  //   if( (event.keyCode && event.keyCode == 27) || (event.key && event.key.toLowerCase() == 'escape' )) {
-  //     // close navigation on mobile if open
-  //     if(trigger.getAttribute('aria-expanded') == 'true' && isVisible(trigger)) {
-  //       focusMenu = trigger; // move focus to menu trigger when menu is close
-  //       trigger.click();
-  //     }
-  //   }
-  //   // listen for tab key
-  //   if( (event.keyCode && event.keyCode == 9) || (event.key && event.key.toLowerCase() == 'tab' )) {
-  //     // close navigation on mobile if open when nav loses focus
-  //     if(trigger.getAttribute('aria-expanded') == 'true' && isVisible(trigger) && !document.activeElement.closest('.js-header')) trigger.click();
-  //   }
-  // });
+  const nav = document.querySelector(".header .nav")
 
 
+  nav.addEventListener('click', (e) => toggleMenu())
 
+  // listen for key events
+  window.addEventListener('keyup', function(event){
+    // listen for esc key
+    if( (event.key && event.key == 27) || (event.key && event.key.toLowerCase() == 'escape' )) {
+      // close navigation on mobile if open
+      if(nav.classList.contains("open")) {
+        // focusMenu = trigger; // move focus to menu trigger when menu is closed
+        nav.classList.toggle('open');
+      }
+    }
+
+
+    // listen for tab key
+    // if( (event.key && event.key== 9) || (event.key && event.key.toLowerCase() == 'tab' )) {
+    //   // close navigation on mobile if open when nav loses focus
+    //   if(trigger.getAttribute('aria-expanded') == 'true' && isVisible(trigger) && !document.activeElement.closest('.js-header')) trigger.click();
+    // }
+  });
+
+
+    const toggleMenu = () => {
+      nav.classList.toggle("open");
+    }

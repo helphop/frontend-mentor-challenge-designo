88954314709f1553ffe902db1d03dd9afe9bdcb3
Mitchell Gould <mitchellgould7@gmail.com>
2021-02-26 13:55:44 -0500

    add aria identifiers

--- main/assets/js/components/_navigation.js
+++ main/assets/js/components/_navigation.js
@@ -1,28 +1,34 @@
-  const nav = document.querySelector(".header .nav")
+const nav = document.querySelector(".header .nav")
+const openTrigger = nav.querySelector(".nav__icon--open")
+const closeTrigger = nav.querySelector(".nav__icon--close")
+const navList = document.querySelector(".nav__list")
+let isOpen = false;
 
+nav.addEventListener('click', (e) => {
+  toggleMenu()
+  setMenuState();
+})
 
-  nav.addEventListener('click', (e) => toggleMenu())
-
-  // listen for key events
-  window.addEventListener('keyup', function(event){
-    // listen for esc key
-    if( (event.key && event.key == 27) || (event.key && event.key.toLowerCase() == 'escape' )) {
-      // close navigation on mobile if open
-      if(nav.classList.contains("open")) {
-        // focusMenu = trigger; // move focus to menu trigger when menu is closed
-        nav.classList.toggle('open');
-      }
+// listen for key events
+window.addEventListener('keyup', function(event){
+  // listen for esc key
+  if( (event.key && event.key == 27) || (event.key && event.key.toLowerCase() == 'escape' )) {
+    // close navigation on mobile if open
+    if(isOpen === true) {
+      // focusMenu = trigger; // move focus to menu trigger when menu is closed
+      toggleMenu();
+      setMenuState();
+      openTrigger.focus();
     }
+  }
+
+  // listen for tab key
+  if( (event.key && event.key== 9) || (event.key && event.key.toLowerCase() == 'tab' )) {
+    // close navigation on mobile if open when nav loses focus
+    if(trigger.getAttribute('aria-expanded') == 'true' && isVisible(trigger) ) trigger.click();
+  }
+});
 
 
-    // listen for tab key
-    // if( (event.key && event.key== 9) || (event.key && event.key.toLowerCase() == 'tab' )) {
-    //   // close navigation on mobile if open when nav loses focus
-    //   if(trigger.getAttribute('aria-expanded') == 'true' && isVisible(trigger) && !document.activeElement.closest('.js-header')) trigger.click();
-    // }
-  });
-
-
-    const toggleMenu = () => {
-      nav.classList.toggle("open");
-    }
+const toggleMenu = () => nav.classList.toggle("open")
+const setMenuState = () => isOpen = nav.classList.contains('open')

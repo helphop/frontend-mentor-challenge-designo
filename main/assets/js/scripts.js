825d54ab31d86b8dc612fd207351c9fbaa912cca
Mitchell Gould <mitchellgould7@gmail.com>
2021-02-26 14:48:20 -0500

    working menu with some accessibility set

--- main/assets/js/scripts.js
+++ main/assets/js/scripts.js
@@ -338,20 +338,12 @@ window.addEventListener('keyup', function(event){
   if( (event.key && event.key == 27) || (event.key && event.key.toLowerCase() == 'escape' )) {
     // close navigation on mobile if open
     if(isOpen === true) {
-      // focusMenu = trigger; // move focus to menu trigger when menu is closed
       toggleMenu();
       setMenuState();
-      openTrigger.focus();
     }
   }
 
-  // listen for tab key
-  if( (event.key && event.key== 9) || (event.key && event.key.toLowerCase() == 'tab' )) {
-    // close navigation on mobile if open when nav loses focus
-    if(trigger.getAttribute('aria-expanded') == 'true' && isVisible(trigger) ) trigger.click();
-  }
 });
-
 
 const toggleMenu = () => nav.classList.toggle("open")
 const setMenuState = () => isOpen = nav.classList.contains('open')
